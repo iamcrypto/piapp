9635364ad48d10742a02e6ec8958989b6e228b98
iamlive <71422116+iamcrypto@users.noreply.github.com>
2024-11-26 07:40:03 +0530

    Update index.js
    added

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const io = socket(server)
 var players;
 var joined = true;
 
-var serviceAccount = require("/root/piapp/nodejs.json");
+var serviceAccount = require("../piapp/nodejs.json");
 
 
 admin.initializeApp({
