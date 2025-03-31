e0d25cb2bc6e4e4c02d9d39b415727cdac915af6
iamlive <71422116+iamcrypto@users.noreply.github.com>
2025-03-31 08:42:26 +0530

    commit
    alphabet

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const cron = require('node-cron');
 const http = require('http');
 const socketIo = require('socket.io');
+const cron = require('node-cron');
 
 const app = express();
 const server = http.createServer(app);
@@ -12,15 +12,29 @@ const PORT = 3000;
 // Serve static frontend files
 app.use(express.static('public'));
 
+let currentLetterIndex = 0;
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
 // WebSocket connection
 io.on('connection', (socket) => {
   console.log('A user connected via WebSocket.');
+
+  // Send the current letter to the newly connected user
+  if (currentLetterIndex > 0) {
+    socket.emit('letter', alphabet[currentLetterIndex - 1]);
+  }
 });
 
-// Schedule a cron job to notify the frontend every minute
+// Schedule a cron job to emit letters every minute
 cron.schedule('*/1 * * * *', () => {
-  console.log('Cron job triggered!');
-  io.emit('alert', 'This is a scheduled alert from the backend!');
+  if (currentLetterIndex < alphabet.length) {
+    const letter = alphabet[currentLetterIndex];
+    console.log(`Sending letter: ${letter}`);
+    io.emit('letter', letter);
+    currentLetterIndex++;
+  } else {
+    console.log('All letters have been sent!');
+  }
 });
 
 // Start the server
